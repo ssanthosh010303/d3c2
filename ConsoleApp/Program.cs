a299a189c5f0a0d3a5e51bf2bd2e623a12b140d7
Sakthi Santhosh <116057110+sakthisanthosh010303@users.noreply.github.com>
2024-04-17 07:29:08 +0100

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Sakthi (Sandy) Santhosh
+ * Created on: 11/04/2024
+ *
+ * Day-3 Challenge-2: Find the Largest Number
+ */
+namespace Challenge2;
+
+class Program
+{
+    static void Main()
+    {
+        int largestNumber = int.MinValue;
+
+        int number;
+        string? inputData;
+
+        Console.Write("Enter number(s): ");
+        inputData = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(inputData)) return;
+
+        foreach (string numberString in inputData.Split(' '))
+        {
+            number = int.Parse(numberString);
+            if (number > largestNumber) largestNumber = number;
+        }
+
+        Console.WriteLine("\nResult");
+        Console.WriteLine("  Largest number: " + largestNumber);
+    }
+}
